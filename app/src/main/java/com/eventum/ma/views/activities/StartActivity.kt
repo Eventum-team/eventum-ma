bca863f3f1a5111d3496e9c9912789d9ebd7ceb5
Diego <juanmedina0505@gmail.com>
2020-04-27 21:32:01 -0500

    Initial Commit :tada::tada::tada:

---
+++ app/src/main/java/com/eventum/ma/views/activities/StartActivity.kt
@@ -0,0 +1,13 @@
+package com.eventum.ma.views.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.eventum.ma.R
+
+class StartActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+    }
+}
